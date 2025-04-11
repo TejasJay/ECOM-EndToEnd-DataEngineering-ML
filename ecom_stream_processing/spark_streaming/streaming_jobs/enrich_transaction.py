3816beac2d01cfd4225df0195889f42e184ae50b
TejasJay <tejasjayprakash@gmail.com>
2025-04-11 19:18:01 -0400

    fixing network & topic issue

--- ecom_stream_processing/spark_streaming/streaming_jobs/enrich_transaction.py
+++ ecom_stream_processing/spark_streaming/streaming_jobs/enrich_transaction.py
@@ -1,5 +1,4 @@
 import yaml
-import os
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import col, from_json
@@ -8,31 +7,20 @@ from pyspark.sql.functions import col, from_json
 with open("config/streaming_config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-
-
 # Config values
 app_name = config["app_name"]
 brokers = config["kafka"]["brokers"]
-transaction_topic = config["kafka"]["topics"]["transactions"]
 user_topic = config["kafka"]["topics"]["users"]
 starting_offsets = config["kafka"]["starting_offsets"]
 output_mode = config["output"]["mode"]
 truncate = config["output"]["truncate"]
 
 print(f"🧪 Kafka brokers being used: {brokers}")
+print(f"📡 Subscribing to topic: {user_topic}")
 
 # Spark session
 spark = SparkSession.builder.appName(app_name).getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
-
-# Transaction schema
-transaction_schema = StructType([
-    StructField("transaction_id", StringType()),
-    StructField("user_id", StringType()),
-    StructField("item_id", StringType()),
-    StructField("amount", DoubleType()),
-    StructField("timestamp", TimestampType())
-])
 
 # User schema
 user_schema = StructType([
@@ -54,17 +42,6 @@ user_schema = StructType([
     StructField("persona", StringType())
 ])
 
-# Read transaction stream
-transactions_df = spark.readStream.format("kafka") \
-    .option("kafka.bootstrap.servers", brokers) \
-    .option("subscribe", transaction_topic) \
-    .option("startingOffsets", starting_offsets) \
-    .load()
-
-transactions_parsed = transactions_df.selectExpr("CAST(value AS STRING)") \
-    .select(from_json(col("value"), transaction_schema).alias("data")) \
-    .select("data.*")
-
 # Read user stream
 users_df = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", brokers) \
@@ -76,18 +53,20 @@ users_parsed = users_df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), user_schema).alias("data")) \
     .select("data.*")
 
-# Output both to console
-transactions_query = transactions_parsed.writeStream \
-    .format(output_mode) \
-    .option("truncate", truncate) \
-    .queryName("TransactionStream") \
-    .start()
-
+print("✅ Starting user stream...")
 users_query = users_parsed.writeStream \
-    .format(output_mode) \
+    .format("console") \
+    .outputMode(output_mode) \
     .option("truncate", truncate) \
     .queryName("UserStream") \
     .start()
 
-transactions_query.awaitTermination()
-users_query.awaitTermination()
+# 🚫 Transaction stream removed since topic doesn't exist
+# Uncomment below once the topic is created
+# transaction_topic = config["kafka"]["topics"]["transactions"]
+# transaction_schema = StructType([...])
+# transactions_df = ...
+# transactions_parsed = ...
+# transactions_query = ...
+
+spark.streams.awaitAnyTermination()

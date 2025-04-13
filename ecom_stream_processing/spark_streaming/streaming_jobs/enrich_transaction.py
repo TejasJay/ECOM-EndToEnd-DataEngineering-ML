c42c24584a08664ee1aa3327970a17fa94490d12
TejasJay <tejasjayprakash@gmail.com>
2025-04-12 21:39:23 -0400

    change schema json

--- ecom_stream_processing/spark_streaming/streaming_jobs/enrich_transaction.py
+++ ecom_stream_processing/spark_streaming/streaming_jobs/enrich_transaction.py
@@ -2,13 +2,12 @@ import yaml
 import json
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType
-from pyspark.sql.functions import col, from_json
+from pyspark.sql.functions import col, from_json, to_timestamp
 
-# Load YAML config
+# Load config
 with open("config/streaming_config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-# Config values
 app_name = config["app_name"]
 brokers = config["kafka"]["brokers"]
 user_topic = config["kafka"]["topics"]["users"]
@@ -21,34 +20,37 @@ print(f"🧪 Kafka brokers being used: {brokers}")
 print(f"📡 Subscribing to topic: {user_topic}")
 print(f"⏳ Starting offset: {starting_offsets}")
 
-# Initialize Spark
-spark = SparkSession.builder.appName(app_name).getOrCreate()
-spark.sparkContext.setLogLevel("WARN")
-
-# Load user schema from JSON
+# Load schema
 with open(user_schema_path, "r") as f:
     user_schema_json = json.load(f)
 
 user_schema = StructType.fromJson(user_schema_json)
 
-# Read user stream
+# Create Spark session
+spark = SparkSession.builder.appName(app_name).getOrCreate()
+spark.sparkContext.setLogLevel("WARN")
+
+# Read Kafka stream
 users_df = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", brokers) \
     .option("subscribe", user_topic) \
     .option("startingOffsets", starting_offsets) \
     .load()
 
-# Parse Kafka JSON messages
+# Parse value
 users_parsed = users_df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), user_schema).alias("data")) \
-    .select("data.*")
+    .select("data.*") \
+    .withColumn("account_creation_date", to_timestamp("account_creation_date")) \
+    .withColumn("last_login_time", to_timestamp("last_login_time"))
 
-# Output to console
+# Write to console
 print("✅ Starting user stream...")
 users_query = users_parsed.writeStream \
     .format("console") \
     .outputMode(output_mode) \
     .option("truncate", truncate) \
+    .option("numRows", 5) \
     .queryName("UserStream") \
     .start()
 
